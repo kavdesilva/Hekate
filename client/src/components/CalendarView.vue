65b9adf12e5ef8b26343af04e731ca1797131903
Kristina <kavdesilva@gmail.com>
2022-09-09 18:23:28 -0400

    more style

--- client/src/components/CalendarView.vue
+++ client/src/components/CalendarView.vue
@@ -14,7 +14,8 @@
             :maxDate="new Date()"
             :enableTimePicker="false"
             :dayNames="['m', 'tu', 'w', 'th', 'f', 'sa', 'su']"
-            :monthChangeOnScroll="false">
+            :monthChangeOnScroll="false"
+            menuClassName="dp-custom-menu">
         </DatePicker>
         <h3 v-if="date">selected date: {{ date.toDateString().toLowerCase() }}</h3>
         <button v-if="date">add record</button>
@@ -41,6 +42,8 @@ import '@vuepic/vue-datepicker/src/VueDatePicker/style/main.scss';
         width: min-content;
         padding-bottom: 20px;
         text-transform: lowercase;
+        border: 1px solid whitesmoke;
+        border-radius: 4px;
     }
     .dp__month_year_select {
         font-size: 30px;
@@ -50,9 +53,16 @@ import '@vuepic/vue-datepicker/src/VueDatePicker/style/main.scss';
     }
     .dp__now_button {
         font-size: 20px;
-        padding: 5px 10px;
+        color: whitesmoke;
+        border-color: rgb(224, 223, 223);
+        font-family: Georgia, 'Times New Roman', Times, serif;
+        font-weight: 500;
     }
     .dp__cell_disabled {
         color: rgb(97, 96, 96);
+    }
+    .dp-custom-menu {
+        font-family: Georgia, 'Times New Roman', Times, serif;
+        border: none;
     }
 </style>
